2ff7da8c9235a8dee1bcedc56f490169a62ff322
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2025-06-10 08:53:35 +0300

    Update patient_doc_generator.py

--- patient_doc_generator.py
+++ patient_doc_generator.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from google import genai
-from google.genai import types as genai_types
+import google.generativeai as genai
 import json
 from io import BytesIO
 from docxtpl import DocxTemplate
